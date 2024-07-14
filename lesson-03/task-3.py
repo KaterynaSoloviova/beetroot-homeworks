161ffe5fec03b7fd0770134caa5533c654fc7c5a
Kateryna Soloviova <kateryna.soloviova@gmail.com>
2024-07-14 22:16:07 +0200

    add tasks from lesson 3

---
+++ lesson-03/task-3.py
@@ -0,0 +1,22 @@
+# Task 3
+
+# Using python as a calculator.
+# Make a program with 2 numbers saved in separate variables a and b, then print the result for each of the following:
+# Addition
+# Subtraction
+# Division
+# Multiplication
+# Exponent (Power)
+# Modulus
+# Floor division
+
+num1 = 73
+num2 = 3.14
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Exponent:", num1 ** num2)
+print("Modulus:", num1 % num2)
+print("Floor division:", num1 // num2)
