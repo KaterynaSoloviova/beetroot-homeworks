eaf8e19ba27ce98e959c0a2eed0fcc75a67fd465
Kateryna Soloviova <kateryna.soloviova@gmail.com>
2024-08-07 20:27:51 +0200

    Add tasks from lesson-9

---
+++ lesson-09/task-1.py
@@ -0,0 +1,8 @@
+# Task 1
+# Imports practice
+# Make a directory with 2 modules; make a function in one of them; then import this function in the other module and
+# use that in your script of choice.
+
+from temperature.module2 import test_temperature_convert
+
+test_temperature_convert()
