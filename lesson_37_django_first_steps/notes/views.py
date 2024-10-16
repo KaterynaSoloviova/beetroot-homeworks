8b81ba30b7df00c2968b28ba48ad784dba810887
Kateryna Soloviova <kateryna.soloviova@gmail.com>
2024-10-16 20:20:09 +0200

    Add tasks from lesson_38_html_css

--- lesson_37_django_first_steps/notes/views.py
+++ lesson_37_django_first_steps/notes/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse, HttpRequest
-
+from notes.models import Note
 
 # Create your views here.
 
 def notes(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello from Notes app!")
+    notes = Note.objects.order_by("-created_date")
+    template = loader.get_template("notes/index.html")
+    list = []
+    for note in notes:
+        date = note.created_date.strftime("%B %d, %Y")
+        note_item = {
+            "date": date,
+            "text": note.text,
+            "author": note.author,
+        }
+        list.append(note_item)
+    context = {"notes": list}
+    return HttpResponse(template.render(context, request))
+
+
